3f10d7f9ab607158a0e43dc98ee0e38ce0c38f17
prao <pch5521@gmail.com>
2023-11-14 12:36:12 +0900

    feat(EventController): 이벤트 컨트롤러 기능 구현
    - run: 이벤트를 실행하는 메서드
    - getVisitDate: 방문 날짜를 가져온다
    - getInputOrders: 입력받은 주문을 가져온다
    - showPreviewAndOrderList: 날짜와 주문으로 미리보기와 주문 메뉴를 출력한다
    - getTotalOrderPrice: 총주문 금액을 가져온다
    - showTotalOrderPrice: 총주문 금액을 보여준다
    - showPresentMenu: 증정 메뉴를 보여준다
    - showDiscountList: 혜택 내역, 총혜택 금액, 할인 후 예상 결제 금액, 12월 이벤트 배지를 보여준다

---
+++ src/main/java/christmas/controller/EventController.java
@@ -0,0 +1,140 @@
+package christmas.controller;
+
+import christmas.InputValidator;
+import christmas.domain.Badge;
+import christmas.domain.DateDiscount;
+import christmas.domain.DayDiscount;
+import christmas.domain.Menu;
+import christmas.domain.PresentEvent;
+import christmas.domain.SpecialDiscount;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class EventController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public EventController(final InputView inputView, final OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        int date = getVisitDate();
+        Map<String, Integer> orders = getInputOrders();
+        showPreviewAndOrderList(date, orders);
+        int totalOrderPrice = getTotalOrderPrice(orders);
+        showTotalOrderPrice(totalOrderPrice);
+        showPresentMenu(totalOrderPrice);
+        showDiscountList(date, orders, totalOrderPrice);
+    }
+
+    private int getVisitDate() {
+        while (true) {
+            try {
+                int date = inputView.inputVisitDate();
+                InputValidator.validateDate(date);
+                return date;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private Map<String, Integer> getInputOrders() {
+        while (true) {
+            try {
+                Map<String, Integer> orders = inputView.inputOrders();
+                InputValidator.validateMenuNames(orders);
+                Set<String> categories = getCategoriesFromOrders(orders);
+                InputValidator.validateOnlyJuice(categories);
+                int orderCount = orders.values().stream()
+                        .mapToInt(Integer::intValue)
+                        .sum();
+                InputValidator.validateMaxMenuCount(orderCount);
+                InputValidator.validateOrder(orders);
+                return orders;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private  Set<String> getCategoriesFromOrders(Map<String, Integer> orders) {
+        return orders.keySet().stream()
+                .map(Menu::findMenuByName)
+                .filter(Objects::nonNull)
+                .map(Menu::getCategory)
+                .collect(Collectors.toSet());
+    }
+
+    private void showPreviewAndOrderList(final int date, final Map<String, Integer> orders) {
+        outputView.printPreview(date);
+        outputView.printOrderList(orders);
+    }
+
+    private int getTotalOrderPrice(Map<String, Integer> orders) {
+        int totalOrderPrice = 0;
+
+        for (Map.Entry<String, Integer> entry : orders.entrySet()) {
+            String stringName = entry.getKey();
+            int quantity = entry.getValue();
+
+            Menu menu = Menu.findMenuByName(stringName);
+            if (menu != null) {
+                int menuPrice = menu.getMenuPrice();
+                totalOrderPrice += menuPrice * quantity;
+            }
+        }
+
+        return totalOrderPrice;
+    }
+
+    private void showTotalOrderPrice(final int totalOrderPrice) {
+        outputView.printTotalOrderPrice(totalOrderPrice);
+    }
+
+    private void showPresentMenu(final int totalOrderPrice) {
+        String present = getPresent(totalOrderPrice);
+        outputView.printPresent(present);
+    }
+
+    private String getPresent(final int totalOrderPrice) {
+        return PresentEvent.getPresent(totalOrderPrice);
+    }
+
+    private void showDiscountList(final int date, final Map<String, Integer> orders, final int totalOrderPrice) {
+        int dateDiscount = getDateDiscount(date);
+        int dayDiscount = getDayDiscount(date, orders);
+        int specialDiscount = getSpecialDiscount(date);
+        String present = getPresent(totalOrderPrice);
+        int presentPrice = 0;
+        if (!present.equals("없음")) {
+            presentPrice = Menu.findMenuByName(present).getMenuPrice();
+        }
+        int totalDiscount = outputView.printTotalDiscount(dateDiscount, dayDiscount, specialDiscount, present);
+        outputView.printAfterDiscount(totalOrderPrice, totalDiscount, presentPrice);
+        String badge = getEventBadge(totalDiscount);
+        outputView.printBadge(badge);
+    }
+
+    private int getDateDiscount(final int date) {
+        return DateDiscount.getDateDiscount(date);
+    }
+
+    private int getDayDiscount(final int date, Map<String, Integer> orders) {
+        return DayDiscount.getDayDiscount(date, orders);
+    }
+
+    private int getSpecialDiscount(final int date) {
+        return SpecialDiscount.getSpecialDiscount(date);
+    }
+
+    private String getEventBadge(final int totalDiscount) {
+        return Badge.getBadge(totalDiscount);
+    }
+}
