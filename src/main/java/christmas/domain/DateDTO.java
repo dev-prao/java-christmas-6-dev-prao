820a7d971e8232f92616f1328fea33bce91c5ad3
prao <pch5521@gmail.com>
2023-11-14 15:48:15 +0900

    feat(DateDTO): 방문 날짜 DTO 생성
    - 기존의 InputValidator 제거 후 검증 로직을 DTO로 이동
    - 유효성 검사를 진행 후 int date를 전달하는 dto 생성

---
+++ src/main/java/christmas/domain/DateDTO.java
@@ -0,0 +1,20 @@
+package christmas.domain;
+
+public record DateDTO(
+        int date
+) {
+    private static final String ERROR_FORMAT = "[ERROR] %s";
+    private static final String INVALID_VISIT_DATE = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    private static final int MIN_DATE = 1;
+    private static final int MAX_DATE = 31;
+
+    public DateDTO {
+        validateDate(date);
+    }
+
+    private void validateDate(final int date) {
+        if (date < MIN_DATE || date > MAX_DATE) {
+            throw new IllegalArgumentException(String.format(ERROR_FORMAT, INVALID_VISIT_DATE));
+        }
+    }
+}
