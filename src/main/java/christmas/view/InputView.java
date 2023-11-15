f0d6a6f5bc76ef9ed2c3e35d9391a8c4bfb71d0c
prao <pch5521@gmail.com>
2023-11-15 13:30:06 +0900

    style(View): 공백, 빈줄, 오탈자 수정

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -18,7 +18,7 @@ public class InputView {
     }
 
     public Map<String, Integer> inputOrders() {
-        System.out.println("주문하실 메뉴의 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         return Parser.parseOrder(Console.readLine());
     }
 }
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -7,7 +7,7 @@ public class OutputView {
     private static final String PREVIEW_FORMAT = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
     private static final String PRINT_ORDER_FORMAT = "%s %d개";
     private static final DecimalFormat MONEY_FORMAT = new DecimalFormat("#,###");
-    private static final String TOTAL_DISCOUNT_FORMAT = "크리스마스 디데이 할인: -%s원\n평일 할인: -%s원\n특별 할인: -%s원\n증정 이벤트: -%s원\n\n<총혜택 금액>\n-%s원\n";
+    private static final String TOTAL_DISCOUNT_FORMAT = "크리스마스 디데이 할인: -%s원\n평일 할인: -%s원\n특별 할인: -%s원\n증정 이벤트: -%s원\n\n<총혜택 금액>\n-%s원";
     private static final String TOTAL_NO_DISCOUNT_FORMAT = "없음\n\n<총혜택 금액>\n0원";
 
     public void printPreview(final int date) {
@@ -75,6 +75,6 @@ public class OutputView {
 
     public void printBadge(final String badge) {
         System.out.println("\n<12월 이벤트 배지>");
-        System.out.println(badge);
+        System.out.print(badge);
     }
 }
